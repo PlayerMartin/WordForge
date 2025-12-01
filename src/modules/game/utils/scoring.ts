87ba4dfcfb8cb359ac8663bedb0bb8919b6218a8
Jakub Majer <567782@mail.muni.cz>
2025-12-01 16:44:13 +0100

    fix: remove scoring mode from scoring util

--- src/modules/game/utils/scoring.ts
+++ src/modules/game/utils/scoring.ts
@@ -1,6 +1,5 @@
 // modules/game/utils/scoring.ts
 import { GAME_SCORING } from "@/modules/game/config/constants";
-import { ScoringMode } from "@/modules/game/config/modes";
 
 
 export const getLengthModeScore = (word: string): number => {
@@ -14,19 +13,4 @@ export const getLengthModeScore = (word: string): number => {
 
 export const getTempoModeScore = (params: {}): number => {
   return GAME_SCORING.TEMPO_POINTS_PER_WORD;
-};
-
-
-export const getScoreForWord = (
-  scoringMode: ScoringMode,
-  word: string
-): number => {
-  switch (scoringMode) {
-    case "length":
-      return getLengthModeScore(word);
-    case "tempo":
-      return getTempoModeScore({});
-    default:
-      return 0;
-  }
 };
