116b2f52ddc0254a27b492db38ead39d9007aa36
xgerza <xgerza@fi.muni.cz>
2025-12-04 09:46:49 +0100

    refactor: clean code in types folder and eslint rules added

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,2 @@
-// ============================================
-// TYPES INDEX - Export všetkých typov
-// ============================================
-//
-// Centrálny export pre všetky typy
-// Použitie: import { GameState, UserProfile } from '@/types'
-
 export * from './game'
 export * from './user'
-export * from './leaderboard'
-export * from './api'
