cd6989cc4e9939dd3d3bef6a22b432557f2a1356
dusan <dusan@humanit.cz>
2025-12-06 00:24:42 +0100

    FIX: Next15 changes

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -3,15 +3,18 @@ import { Suspense } from 'react';
 import GameViewport from '@/modules/game/components/game-view-port';
 
 import LoadingGame from './loading';
-
+//This was necesery change for next 15 pls review
 type PageProps = {
-	params: { id: string };
+	params: Promise<{ id: string }>;
 };
 
-const GamePage = ({ params }: PageProps) => (
-	<Suspense fallback={<LoadingGame />}>
-		<GameViewport gameId={params.id} />
-	</Suspense>
-);
+const GamePage = async ({ params }: PageProps) => {
+	const { id } = await params;
+	return (
+		<Suspense fallback={<LoadingGame />}>
+			<GameViewport gameId={id} />
+		</Suspense>
+	);
+};
 
 export default GamePage;
