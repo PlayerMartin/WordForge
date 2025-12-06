0a026be6c21ffaa7192bffb2434026529c6d79ba
Martin Mackovič <xmackov1@fi.muni.cz>
2025-12-06 12:26:34 +0100

    add LanguageButton

--- src/modules/home/components/language-section.tsx
+++ src/modules/home/components/language-section.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import { SUPPORTED_LANGUAGES } from '@/modules/game/config/constants';
-import Card from '../../../components/ui/card';
 import { useLanguageContext } from '@/components/utils/language-provider';
+import { LanguageButton } from './language-button';
 
 const LanguageSection = () => {
 	const { language, setLanguage } = useLanguageContext();
@@ -14,38 +14,16 @@ const LanguageSection = () => {
 			</h2>
 			<div className="flex justify-center gap-4">
 				{Object.values(SUPPORTED_LANGUAGES).map(lang => (
-					<button
+					<LanguageButton
 						key={lang.code}
+						{...lang}
+						selected={lang.code === language}
 						onClick={() => {
 							if (lang.enabled) {
 								setLanguage(lang.code);
 							}
 						}}
-						disabled={!lang.enabled}
-					>
-						<Card
-							padding="sm"
-							className={`px-6 transition-all ${
-								lang.code === language
-									? 'scale-105 border-2 border-primary-500 bg-surface-800 ring-1 ring-primary-400/30'
-									: 'border border-surface-900 bg-surface-900'
-							} ${
-								lang.enabled
-									? ''
-									: 'opacity-40 saturate-50 backdrop-blur-[1px]'
-							} `}
-							key={lang.code}
-						>
-							<span className="mr-2 font-bold text-primary-400">
-								{lang.code}
-							</span>
-							<span
-								className={`font-medium text-white ${lang.enabled ? '' : 'text-opacity-50'}`}
-							>
-								{lang.name}
-							</span>
-						</Card>
-					</button>
+					/>
 				))}
 			</div>
 		</div>
